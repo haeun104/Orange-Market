ff4864c22699e261efa25ef66911dc8f82a601e6
haeun104 <haeun104@gmail.com>
2024-04-23 18:43:24 +0200

    Added leave chat button

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -1,13 +1,15 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import ChatHistoryPerUser from "../components/chat/ChatHistoryPerUser";
 import ChatInput from "../components/chat/ChatInput";
 import { useEffect, useState } from "react";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../firebase/firebase-config";
+import { IoIosArrowDropleftCircle } from "react-icons/io";
 
 const ChatRoom = () => {
   const { partner } = useParams();
   const [partnerName, setPartnerName] = useState("");
+  const navigate = useNavigate();
 
   // Get chat partner's nickname
   useEffect(() => {
@@ -28,14 +30,27 @@ const ChatRoom = () => {
     }
   }, [partner]);
 
+  const handleLeaveChat = () => {
+    navigate(-1);
+  };
+
   if (partner) {
     return (
-      <div className="max-w-[600px] px-4 mx-auto">
+      <div className="max-w-[600px] px-4 mx-auto relative">
         <h2 className="text-lg font-bold text-center mb-8">
           Chat with {partnerName}
         </h2>
         <ChatHistoryPerUser chatPartner={partner} />
         <ChatInput chatPartner={partner} />
+        <div
+          className="flex items-center text-accent-grey cursor-pointer group absolute top-0"
+          onClick={handleLeaveChat}
+        >
+          <IoIosArrowDropleftCircle size={24} />
+          <span className="text-sm group-hover:decoration-solid group-hover:underline group-hover:text-black">
+            Leave Chat
+          </span>
+        </div>
       </div>
     );
   }
